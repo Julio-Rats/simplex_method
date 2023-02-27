7ffb0ddfb62c4c7cf7a5aed0d4dc804400bbf4e9
Júlio César de Melo Cândido <juliocesarifmg@gmail.com>
2023-02-27 20:03:59 -0300

    Data input
    Program receives file name via command

--- main.c
+++ main.c
@@ -5,13 +5,20 @@
 #include "input.h"
 #include "simplex.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
     setlocale(LC_NUMERIC, "Portuguese");
     setlocale(LC_CTYPE, "Portuguese");
     srand(time(NULL));
-    input_file("teste.txt");
-    simplex();
 
-    return 0;
+    if (argc < 2)
+    {
+        printf("\nPor favor, forneça o nome do arquivo a ser lido.\n\n");
+        return EXIT_FAILURE;
+    }
+
+    input_file(argv[1]);
+    simplex();
+    
+    return EXIT_SUCCESS;
 }
